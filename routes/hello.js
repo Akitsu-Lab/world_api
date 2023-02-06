27bc37248252bf0a995f274d68ecc0eca4b77e49
SOTA <snssota@gmail.com>
2023-02-06 20:58:54 +0900

    クエリパラメータを利用

--- routes/hello.js
+++ routes/hello.js
@@ -1,29 +1,51 @@
 const express = require("express");
 const router = express.Router();
 
+// curl http://localhost:3001/hello?name=sota&mail=sota@gmail.com
 router.get("/", (req, res, next) => {
-  var data = {
-    title: `Hello!`,
-    content: "これは、サンプルのコンテンツです。<br>this is sample.",
-  };
-  res.render("hello", data);
+    var name = req.query.name;
+    var mail = req.query.mail;
+    var data = {
+        title: 'Hello!',
+        content: `あなたの名前は、` + name + '。<br>' + `メールアドレスは` + mail + 'です。'
+    };
+    res.render("hello", data);
 });
 
-router.get("/get", (req, res, next) => {
-  var data = {
-    title: `Hello!`,
-    content: "これは、サンプルのコンテンツです。<br>this is sample.",
-  };
-  res.send(data);
-});
+// curl http://localhost:3001/hello/form
+router.get('/form', (req, res, next) => {
+    var data = {
+        title: 'Hello',
+        content: '*何か書いて送信してください'
+    };
+    res.send(data);
+})
 
-// curl -X POST -H "Content-Type: application/json" -d '{"name":"太郎", "age":"30"}' http://localhost:3001/hello/get
-router.post("/get", (req, res, next) => {
-  var data = {
-    title: req.body.name,
-    content: req.body.age,
-  };
-  res.send(200, data);
-});
+// curl http://localhost:3001/hello/form -X POST -H "Content-Type: application/json" -d '{"message":"Node.jsのExpressはメジャーなフレームワークである。"}'
+router.post('/form', (req, res, next) => {
+    var msg = req.body['message'];
+    // var msg = req.body.message;
+    var data = {
+        title: 'Hello!',
+        content: 'あなたは、「' + msg + `」と送信しました。`
+    }
+    res.send(data);
+})
+// router.get("/get", (req, res, next) => {
+//   var data = {
+//     title: `Hello!`,
+//     content: "これは、サンプルのコンテンツです。<br>this is sample.",
+//   };
+//   res.send(data);
+// });
+
+// // curl -X POST -H "Content-Type: application/json" -d '{"name":"太郎", "age":"30"}' http://localhost:3001/hello/get
+// router.post("/get", (req, res, next) => {
+//   var data = {
+//     title: req.body.name,
+//     content: req.body.age,
+//   };
+//   res.send(200, data);
+// });
 
 module.exports = router;
